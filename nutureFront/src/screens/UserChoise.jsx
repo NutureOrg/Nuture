1cf5ae3e7e7d667989b9a6841581cc25577d0515
doguebart <douglasweelber.2004@outlook.com>
2023-06-02 18:58:55 -0300

    creating UserChoise screen

--- nutureFront/src/screens/UserChoise.jsx
+++ nutureFront/src/screens/UserChoise.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Container } from './styles';
+import React from "react";
+import { Container, Background } from "./styles";
+import { useNavigation } from '@react-navigation/native';
+
+import Title from "../components/title/Title";
+import Button from "../components/button/Button";
 
 const UserChoise = () => {
-    return (
-        <Container>
-            
-        </Container>
-    )
-}
+  const navigation = useNavigation();
+
+  const isClient = () => {
+    navigation.navigate('');
+  }
+
+  const isDistribuitor = () => {
+    navigation.navigate('');
+  }
+
+  return (
+    <Container>
+      <Background source={require("../assets/BackgroundImage.jpg")}>
+        <Title text="Como deseja utilizar nosso app?" />
+        <Button>Sou Cliente</Button>
+        <Button>Centro de Distribuição</Button>
+      </Background>
+    </Container>
+  );
+};
 
 export default UserChoise;
