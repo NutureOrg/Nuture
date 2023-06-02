242fefc8e77c26220324cc67d27f2a3f4b3bcd9d
doguebart <douglasweelber.2004@outlook.com>
2023-06-02 18:40:53 -0300

    creating folders setup

---
+++ nutureFront/src/screens/UserChoise.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+const UserChoise = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default UserChoise;
